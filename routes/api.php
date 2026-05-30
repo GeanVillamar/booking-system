b81d3512fb689e35e76cc8c4e782f8157070a64d
Gean Villamar <geanfranklinvillamar@gmail.com>
2026-05-29 19:19:08 -0500

    modified routes api

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,13 @@ use Illuminate\Support\Facades\Route;
  * routes are loaded by the RouteServiceProvider within a group which
  * is assigned the "api" middleware group. Enjoy building your API!
  ***************/
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('users', UserController::class);
-});
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::apiResource('users', UserController::class);
+// });
+
+Route::apiResource('users', UserController::class);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('services', ServiceController::class);
 });
